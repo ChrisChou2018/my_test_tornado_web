752fc08dba8a716dce02d889b220e7b40df163ef
ChrisChou <1050434689@qq.com>
2018-04-19 10:11:18 +0800

    删除.DS_Store文件修改static为assets

--- config_local.py
+++ config_local.py
@@ -3,4 +3,4 @@ import os
 os.environ["DB_HOST"] = "localhost"
 os.environ["DB_PASS"] = "123"
 os.environ["DB_USER"] = "root"
-os.environ['STATIC_PATH'] = 'static'
+os.environ['STATIC_PATH'] = 'assets'
