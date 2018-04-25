7559aa14ed141d0ff9cd86f61c05f3646f60da6b
ChrisChou <1050434689@qq.com>
2018-04-25 12:21:42 +0800

    新增用户信息界面，生成用户信息表

--- app/handlers/admin/a_walcome.py
+++ app/handlers/admin/a_walcome.py
@@ -1,11 +1,47 @@
 from app.libs.decorators import admin_authenticated
 from app.libs.handlers import SiteBaseHandler, ApiBaseHandler
-
+from app.models.member_model import Member
+import json
 # /
 class AdminHomeHandler(SiteBaseHandler):
     @admin_authenticated
     def get(self):
         self.render("admin/a_index.html")
+        
+
+class MemberManage(SiteBaseHandler):
+    @admin_authenticated
+    def get(self):
+        self.render("admin/a_member_manage.html")
+
+
+
+def init_table(t_heads,tbodys):
+    thead_str = "<thead><tr>"
+    for i in t_heads:
+        thead_str += "<th>{0}</th>".format(i)
+    else:
+        thead_str += "</tr></thead>"
+    tbody_str = "<tbody>"
+    for i in tbodys:
+        tbody_str += "<tr>"
+        for j in i:
+            tbody_str += "<td>{0}</td>".format(j)
+        tbody_str += "</tr>"
+    else:
+        tbody_str += "/tbody"
+    return thead_str + tbody_str
+    
+
+
+
+class ApiMemberInfoHandler(ApiBaseHandler):
+    def get(self):
+        member_obj = Member.select()
+        data_list = [[i.member_id, i.member_name, i.email, i.role] for i in member_obj]
+        return_data = init_table(['member_id', 'member_name', 'email', 'role'], data_list)
+        self.write({'return_data':return_data})
+        
 
 
 class AdminJsAddJobHandler(ApiBaseHandler):
--- app/handlers/admin/urls.py
+++ app/handlers/admin/urls.py
@@ -10,4 +10,9 @@ urls = [
     (r"/signout/?",                         a_account.AdminSignoutHandler),
     (r"/register/?",                        a_account.AdminRegisterHandler),
     (r"/change_password/?",                 a_account.AdminChangePasswordHandler),
+    (r"/member_manage/?",                   a_walcome.MemberManage),
+]
+
+urls += [
+    (r"/v1/member_info/?",                  a_walcome.ApiMemberInfoHandler),
 ]
