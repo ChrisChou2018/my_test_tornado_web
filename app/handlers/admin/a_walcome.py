e2358d190448ef8d4c1db83ba972a77a6a4da302
ChrisChou <1050434689@qq.com>
2018-05-02 18:26:02 +0800

    移除form文件，把form验证放在各自需要验证的地方上面

--- app/handlers/admin/a_walcome.py
+++ app/handlers/admin/a_walcome.py
@@ -4,18 +4,18 @@ from app import libs
 from app.models import member_model
 import json
 import config_web
-from app.handlers.admin import form
 import bcrypt
 import random
 import string
 import datetime as dt
+import re
 # /
 class AdminHomeHandler(handlers.SiteBaseHandler):
     @decorators.admin_authenticated
     def get(self):
         self.render("admin/a_index.html")
 
-   
+
 # /member_manage/
 class MemberManage(handlers.SiteBaseHandler):
     @decorators.admin_authenticated
@@ -45,21 +45,76 @@ class AdminJsMemberInfoHandler(handlers.JsSiteBaseHandler):
             filter_args = 'search_value={0}'.format(value)
             search_value = ((member.member_name == value) | (member.email == value))
         except:
+            filter_args = None
             search_value = None
         table_head = ['member_id', 'member_name', 'email', 'role', 'more']
         if not search_value:
             member_obj = member.select().order_by(-member.member_id).paginate(int(current_page), 10)
             member_obj_count = member.select().count()
-            page_obj = libs.Pagingfunc(current_page, member_obj_count)
         else:
             member_obj = member.select().where(search_value).order_by(-member.member_id).paginate(int(current_page), 10)
             member_obj_count = member.select().where(search_value).count()
-            page_obj = libs.Pagingfunc(current_page, member_obj_count, filter_args=filter_args)
+        page_obj = libs.Pagingfunc(current_page, member_obj_count, filter_args=filter_args)
         data_list = [[i.member_id, i.member_name, i.email, i.role] for i in member_obj]
         # self.set_header('Content-Type', 'application/json; charset=UTF-8')
         return_data['data'] = libs.create_html_table(table_head, data_list)
         return_data['page'] = page_obj.create_page_btn()
         self.write(json.dumps(return_data))
+
+
+
+class RegisterForm(object):
+    """
+    注册和编辑用户form验证，默认是添加用户验证如果是编辑用户验证，
+    将check_valid中的is_edit变为true即可
+    """
+    def __init__(self):
+        self.member_name = {'re':r"^.{0,15}$", 'msg':'长度不超过15'}
+        self.email = {'re':r"^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$", 'msg':'邮箱格式不正确'}
+        self.password = {'re':r"^.{0,30}$", 'msg':'密码长度不超过30'}
+        self.password2 = {'re':r"^.{0,30}$", 'msg':'密码长 度不超过30'}
+    
+    def check_valid(self, form_data, is_edit = False):
+        form_dict = self.__dict__
+        clear_data = {}
+        return_data = {
+            'clear_data':None,
+            'error_msg':{},
+            'status':True,
+        }
+        if form_data.get('password') != form_data.get('password2'):
+            return_data['error_msg'] = {'password':'两次密码不一致'}
+        elif not form_data.get('password') or not form_data.get('password2'):
+            if is_edit:
+                pass
+            else:
+                return_data['error_msg'] = {'password':'密码不能为空'}
+        elif not form_data.get('member_name'):
+            if  is_edit:
+                pass
+            else:
+                return_data['error_msg'] = {'member_name':'昵称不能为空'}
+        elif not form_data.get('email'):
+            if is_edit:
+                pass
+            else:
+                return_data['error_msg'] = {'email':'邮箱不能为空'}
+        if return_data['error_msg']:
+            return_data['status'] = False
+            return return_data
+        for key, regular in form_dict.items():
+            post_value = form_data.get(key)
+            # 让提交的数据 和 定义的正则表达式进行匹配
+            if post_value:
+                ret = re.match(regular['re'], post_value)
+                if not ret:
+                    return_data['status'] = False
+                    return_data['error_msg'] = {key:regular['msg']}
+                    return return_data
+                clear_data[key] = post_value
+        else:
+            return_data['clear_data'] = clear_data
+            return return_data
 
 
 class AdminJsRegisterMemberHandler(handlers.JsSiteBaseHandler):
@@ -70,7 +125,7 @@ class AdminJsRegisterMemberHandler(handlers.JsSiteBaseHandler):
     def post(self):
         member = member_model.Member
         form_data = self._build_form_data()
-        form_obj = form.RegisterForm()
+        form_obj = RegisterForm()
         return_data = form_obj.check_valid(form_data)
         if not return_data['status']:
             self.write(json.dumps(return_data))
@@ -149,7 +204,7 @@ class AdminJsEditMemberHandler(handlers.JsSiteBaseHandler):
         member = member_model.Member
         member_id = self.get_argument('member_id', None)
         form_data = self._build_form_data()
-        form_obj = form.RegisterForm()
+        form_obj = RegisterForm()
         return_data = form_obj.check_valid(form_data, is_edit=True)
         if not return_data['status']:
             self.write(json.dumps(return_data))
